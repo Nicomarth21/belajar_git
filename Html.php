835a255e8f9e32114ec13c8b7862e7fbc5d51a58
Nicomarth21 <nicolamarthysia2@gmail.com>
2022-07-06 20:37:29 +0700

    hi

---
+++ Html.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title><?php echo "Kuliah" ?></title>
+    </head>
+    <body>
+        <?php
+            echo "<h3>Pertama kali kuliah<br></h3>";
+            echo "<p><h4>mengikuti kuliah hingga lulus</h4></p>";
+            echo "<p>Saya Mendaftar kuliah pada bulan januari,
+            <br>Saat februari saya membayar pendaftaran,
+            <br>diawal bulan maret tepat tanggal 21
+            <br>pertama kali saya masuk kelas online
+            <br>dan saya tidak menghadirinya</p>"
+        ?> 
+    </body>
+</html>
